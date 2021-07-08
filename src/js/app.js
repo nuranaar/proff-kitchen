4e99625568f8725282cf70e10041c1716172d7ae
nurana <nurana.rustamova@azintelecom.az>
2021-07-08 08:11:05 +0400

    added home page header && slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,14 @@
 'use strict';
 import 'bootstrap/dist/js/bootstrap';
 
-$(function() {
-
-  $(document).on("click", "#alert", function(){
-    alert("clicked");
+$(function () {
+  $(".page-lang .list-inline-item.active").siblings().hide();
+  $(".page-lang .list-inline-item ").click(function (e) {
+    e.preventDefault();
+    $(e.target).addClass("active");
+    $(e.target).siblings().removeClass("active");
+    $(e.target).siblings().toggle(0, function () {
+    });
   });
+
 });
